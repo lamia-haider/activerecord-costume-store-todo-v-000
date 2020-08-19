e8e41c74cf144f2146454d5928d83d0b92d1ac4a
Lamia Haider <lamia.haider@gmail.com>
2020-08-19 21:18:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
+      t.integer :number_employees
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
