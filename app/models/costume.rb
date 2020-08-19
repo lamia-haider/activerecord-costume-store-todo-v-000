29c58dc6b6bf5548911399bdbe3c2ce59ad297ca
Lamia Haider <lamia.haider@gmail.com>
2020-08-19 20:55:09 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,14 +1,3 @@
-class Costume < ActiveRecord::Migration[5.2]
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.string :size
-      t.string :image_url
-      t.timestamps
-    end
-  end
-
-
+class Costume < ActiveRecord::Base
 
 end
